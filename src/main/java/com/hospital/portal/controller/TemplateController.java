7dfaaec865f7fd991b5c6a9ec19421b7d259bfe8
Ibon <castroibon216@gmail.com>
2025-03-26 22:39:06 +0100

    Login and register working

--- src/main/java/com/hospital/portal/controller/TemplateController.java
+++ src/main/java/com/hospital/portal/controller/TemplateController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TemplateController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String showIndex() {
         return "index";
     }
