5af8cc33e31177a6d5e1334483b05e51e972d2d7
Ikerruesgas <i.ruesgas@opendeusto.es>
2025-04-19 18:14:48 +0200

    Implemented DoctorController and DoctorService with multiple functionalities: Added methods to view, create, update, and delete appointments for doctors. Included automatic generation of appointment ID to eliminate manual input.

--- src/main/java/com/hospital/portal/repository/DoctorRepository.java
+++ src/main/java/com/hospital/portal/repository/DoctorRepository.java
@@ -1,7 +1,11 @@
 package com.hospital.portal.repository;
 
 import com.hospital.portal.model.Doctor;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
+	Optional<Doctor> findByDoctorId(String doctorId);
 }
