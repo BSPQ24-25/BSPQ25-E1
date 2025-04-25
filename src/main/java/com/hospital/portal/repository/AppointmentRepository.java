1863a3f096cfc1bcfb581bbb3db016d2190feb28
iboncas <castro.ibon@opendeusto.es>
2025-04-25 19:09:34 +0200

    Performance test

--- src/main/java/com/hospital/portal/repository/AppointmentRepository.java
+++ src/main/java/com/hospital/portal/repository/AppointmentRepository.java
@@ -8,11 +8,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, String> {
-
 	List<Appointment> findByDoctorDni(String dni);
 	List<Appointment> findByPatientDni(String patientDni);
-
 	List<Appointment> findByPatientDniAndDate(String patientDni, LocalDate dateTime);
-    
-
 }
